7349bcaa8188d17cd695385c4a6e8bd567201678
Tobias Salzmann <Eun@users.noreply.github.com>
2019-06-06 13:20:08 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,31 @@
-#!/bin/bash
-printf "\e[1m\e[7m"
-printf "  Welcome! To connect use           \n"
-printf "                                    \n"
-printf "  ssh -X root@%-22s\n" $(hostname -i)
-printf "                                    \n"
+#!/bin/sh
+
+TEXT="\e[0;1;37;45m\e[K"
+CMD="\e[4m"
+HOSTNAME=$(hostname -i)
+
+if [ -z "$RUN_USER" ]; then
+    RUN_USER="root"
+fi
+
+printf ${TEXT}
+printf "  Welcome! To connect use\n"
+printf "\n"
+printf "    ${CMD}ssh -X ${RUN_USER}@${HOSTNAME}${TEXT}\n"
+printf "  OR\n"
+printf "    ${CMD}ssh -X -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${RUN_USER}@${HOSTNAME}${TEXT}\n"
+printf "\n"
 printf "\e[0m\n"
 
-function stop {
+stop() {
     exit 0
 }
 
-trap 'stop' INT
-trap 'stop' SIGINT
+trap 'stop' 1 # SIGHUP
+trap 'stop' 2 # SIGINT
+trap 'stop' 3 # SIGQUIT
+trap 'stop' 6 # SIGABRT
+trap 'stop' 15 # SIGTERM
 
 
 $@
